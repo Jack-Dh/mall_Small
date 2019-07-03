e9170e5fe57baa0b84894f5744a8859c4a0d1f42
Zmyhh <897268593@QQ.com>
2019-07-03 13:08:03 +0800

    样式调整

--- pages/ucenter/index/index.js
+++ pages/ucenter/index/index.js
@@ -4,260 +4,260 @@ var user = require('../../../utils/user.js');
 var app = getApp();
 
 Page({
-  data: {
-    userInfo: {
-      nickName: '点击登录',
-      avatarUrl: 'http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png'
+    data: {
+        userInfo: {
+            nickName: '点击登录',
+            avatarUrl: 'http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png'
+        },
+        userLevel: '基础会员', //用户等级信息（默认基础会员）
+        order: {
+            unpaid: 0,
+            unship: 0,
+            unrecv: 0,
+            uncomment: 0
+        },
+        hasLogin: false
     },
-    userLevel:'',//用户等级信息
-    order: {
-      unpaid: 0,
-      unship: 0,
-      unrecv: 0,
-      uncomment: 0
+    onLoad: function(options) {
+        // 页面初始化 options为页面跳转所带来的参数
     },
-    hasLogin: false
-  },
-  onLoad: function(options) {
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady: function() {
+    onReady: function() {
 
-  },
-  onShow: function() {
+    },
+    onShow: function() {
 
-    //获取用户的登录信息
-    if (app.globalData.hasLogin) {
-      let userInfo = wx.getStorageSync('userInfo'); //获取用户信息，微信封装
-      let userLevel=wx.getStorageSync('userLevel') //获取用户等级
-      
-      this.setData({
-        userInfo: userInfo,
-        hasLogin: true,
-        userLevel:userLevel
-      });
-        console.log(userInfo)
-        console.log(userLevel)
-      let that = this;
-      util.request(api.UserIndex).then(function(res) {
-        if (res.errno === 0) {
-          that.setData({
-            order: res.data.order
-          });
-        }
-  
-      });
-    }
+        //获取用户的登录信息
+        if (app.globalData.hasLogin) {
+            let userInfo = wx.getStorageSync('userInfo'); //获取用户信息，微信封装
+            let userLevel = wx.getStorageSync('userLevel') //获取用户等级
 
-  },
-  onHide: function() {
-    // 页面隐藏
+            this.setData({
+                userInfo: userInfo,
+                hasLogin: true,
+                userLevel: userLevel
+            });
+            console.log(userInfo)
+            console.log(userLevel)
+            let that = this;
+            util.request(api.UserIndex).then(function(res) {
+                if (res.errno === 0) {
+                    that.setData({
+                        order: res.data.order
+                    });
+                }
 
-  },
-  onUnload: function() {
-    // 页面关闭
-  },
-  Sign(){
-      //用户签到
-    /*   util.request(api.AuthLogout, {}, 'POST'); */
-      util.request(api.signIn,{},'POST').then(function(res) {
-        console.log(res)
-        wx.showToast({title:res.data.message})
-  
-      });
-  
-
-  },
-  goLogin() {
-    if (!this.data.hasLogin) {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      })
-    }
-  },
-  goOrder() {
-    if (this.data.hasLogin) {
-      try {
-        wx.setStorageSync('tab', 0);
-      } catch (e) {
-
-      }
-      wx.navigateTo({
-        url: "/pages/ucenter/order/order"
-      });
-    } else {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      });
-    }
-  },
-  goOrderIndex(e) {
-    if (this.data.hasLogin) {
-      let tab = e.currentTarget.dataset.index
-      let route = e.currentTarget.dataset.route
-      try {
-        wx.setStorageSync('tab', tab);
-      } catch (e) {
-
-      }
-      wx.navigateTo({
-        url: route,
-        success: function(res) {},
-        fail: function(res) {},
-        complete: function(res) {},
-      })
-    } else {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      });
-    };
-  },
-  goCoupon() {
-    if (this.data.hasLogin) {
-      wx.navigateTo({
-        url: "/pages/ucenter/couponList/couponList"
-      });
-    } else {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      });
-    };
-  },
-  goGroupon() {
-    if (this.data.hasLogin) {
-      wx.navigateTo({
-        url: "/pages/groupon/myGroupon/myGroupon"
-      });
-    } else {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      });
-    };
-  },
-  goCollect() {
-    if (this.data.hasLogin) {
-      wx.navigateTo({
-        url: "/pages/ucenter/collect/collect"
-      });
-    } else {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      });
-    };
-  },
-  goFeedback(e) {
-    if (this.data.hasLogin) {
-      wx.navigateTo({
-        url: "/pages/ucenter/feedback/feedback"
-      });
-    } else {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      });
-    };
-  },
-  goFootprint() {
-    if (this.data.hasLogin) {
-      wx.navigateTo({
-        url: "/pages/ucenter/footprint/footprint"
-      });
-    } else {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      });
-    };
-  },
-  goAddress() {
-    if (this.data.hasLogin) {
-      wx.navigateTo({
-        url: "/pages/ucenter/address/address"
-      });
-    } else {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      });
-    };
-  },
-  goSign(){
-    //会员中心
-    if (this.data.hasLogin) {
-      wx.navigateTo({
-        url: "/pages/ucenter/sign/sign"
-      });
-    } else {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      });
-    };
-  },
-  bindPhoneNumber: function(e) {
-    if (e.detail.errMsg !== "getPhoneNumber:ok") {
-      // 拒绝授权
-      return;
-    }
-
-    if (!this.data.hasLogin) {
-      wx.showToast({
-        title: '绑定失败：请先登录',
-        icon: 'none',
-        duration: 2000
-      });
-      return;
-    }
-
-    util.request(api.AuthBindPhone, {
-      iv: e.detail.iv,
-      encryptedData: e.detail.encryptedData
-    }, 'POST').then(function(res) {
-      if (res.errno === 0) {
-        wx.showToast({
-          title: '绑定手机号码成功',
-          icon: 'success',
-          duration: 2000
-        });
-      }
-    });
-  },
-  goAfterSale: function() {
-    if (this.data.hasLogin) {
-      wx.navigateTo({
-        url: "/pages/ucenter/afterList/afterList"
-      });
-    } else {
-      wx.navigateTo({
-        url: "/pages/auth/login/login"
-      });
-    };
-  },
-  aboutUs: function() {
-    wx.navigateTo({
-      url: '/pages/about/about'
-    });
-  },
-  goHelp: function () {
-    wx.navigateTo({
-      url: '/pages/help/help'
-    });
-  },  
-  exitLogin: function() {
-    wx.showModal({
-      title: '',
-      confirmColor: '#b4282d',
-      content: '退出登录？',
-      success: function(res) {
-        if (!res.confirm) {
-          return;
+            });
         }
 
-        util.request(api.AuthLogout, {}, 'POST');
-        app.globalData.hasLogin = false;
-        wx.removeStorageSync('token');
-        wx.removeStorageSync('userInfo');
-        wx.reLaunch({
-          url: '/pages/index/index'
-        });
-      }
-    })
+    },
+    onHide: function() {
+        // 页面隐藏
 
-  }
+    },
+    onUnload: function() {
+        // 页面关闭
+    },
+    Sign() {
+        //用户签到
+        /*   util.request(api.AuthLogout, {}, 'POST'); */
+        util.request(api.signIn, {}, 'POST').then(function(res) {
+            console.log(res)
+            wx.showToast({ title: res.data.message })
+
+        });
+
+
+    },
+    goLogin() {
+        if (!this.data.hasLogin) {
+            wx.navigateTo({
+                url: "/pages/auth/login/login"
+            })
+        }
+    },
+    goOrder() {
+        if (this.data.hasLogin) {
+            try {
+                wx.setStorageSync('tab', 0);
+            } catch (e) {
+
+            }
+            wx.navigateTo({
+                url: "/pages/ucenter/order/order"
+            });
+        } else {
+            wx.navigateTo({
+                url: "/pages/auth/login/login"
+            });
+        }
+    },
+    goOrderIndex(e) {
+        if (this.data.hasLogin) {
+            let tab = e.currentTarget.dataset.index
+            let route = e.currentTarget.dataset.route
+            try {
+                wx.setStorageSync('tab', tab);
+            } catch (e) {
+
+            }
+            wx.navigateTo({
+                url: route,
+                success: function(res) {},
+                fail: function(res) {},
+                complete: function(res) {},
+            })
+        } else {
+            wx.navigateTo({
+                url: "/pages/auth/login/login"
+            });
+        };
+    },
+    goCoupon() {
+        if (this.data.hasLogin) {
+            wx.navigateTo({
+                url: "/pages/ucenter/couponList/couponList"
+            });
+        } else {
+            wx.navigateTo({
+                url: "/pages/auth/login/login"
+            });
+        };
+    },
+    goGroupon() {
+        if (this.data.hasLogin) {
+            wx.navigateTo({
+                url: "/pages/groupon/myGroupon/myGroupon"
+            });
+        } else {
+            wx.navigateTo({
+                url: "/pages/auth/login/login"
+            });
+        };
+    },
+    goCollect() {
+        if (this.data.hasLogin) {
+            wx.navigateTo({
+                url: "/pages/ucenter/collect/collect"
+            });
+        } else {
+            wx.navigateTo({
+                url: "/pages/auth/login/login"
+            });
+        };
+    },
+    goFeedback(e) {
+        if (this.data.hasLogin) {
+            wx.navigateTo({
+                url: "/pages/ucenter/feedback/feedback"
+            });
+        } else {
+            wx.navigateTo({
+                url: "/pages/auth/login/login"
+            });
+        };
+    },
+    goFootprint() {
+        if (this.data.hasLogin) {
+            wx.navigateTo({
+                url: "/pages/ucenter/footprint/footprint"
+            });
+        } else {
+            wx.navigateTo({
+                url: "/pages/auth/login/login"
+            });
+        };
+    },
+    goAddress() {
+        if (this.data.hasLogin) {
+            wx.navigateTo({
+                url: "/pages/ucenter/address/address"
+            });
+        } else {
+            wx.navigateTo({
+                url: "/pages/auth/login/login"
+            });
+        };
+    },
+    goSign() {
+        //会员中心
+        if (this.data.hasLogin) {
+            wx.navigateTo({
+                url: "/pages/ucenter/sign/sign"
+            });
+        } else {
+            wx.navigateTo({
+                url: "/pages/auth/login/login"
+            });
+        };
+    },
+    bindPhoneNumber: function(e) {
+        if (e.detail.errMsg !== "getPhoneNumber:ok") {
+            // 拒绝授权
+            return;
+        }
+
+        if (!this.data.hasLogin) {
+            wx.showToast({
+                title: '绑定失败：请先登录',
+                icon: 'none',
+                duration: 2000
+            });
+            return;
+        }
+
+        util.request(api.AuthBindPhone, {
+            iv: e.detail.iv,
+            encryptedData: e.detail.encryptedData
+        }, 'POST').then(function(res) {
+            if (res.errno === 0) {
+                wx.showToast({
+                    title: '绑定手机号码成功',
+                    icon: 'success',
+                    duration: 2000
+                });
+            }
+        });
+    },
+    goAfterSale: function() {
+        if (this.data.hasLogin) {
+            wx.navigateTo({
+                url: "/pages/ucenter/afterList/afterList"
+            });
+        } else {
+            wx.navigateTo({
+                url: "/pages/auth/login/login"
+            });
+        };
+    },
+    aboutUs: function() {
+        wx.navigateTo({
+            url: '/pages/about/about'
+        });
+    },
+    goHelp: function() {
+        wx.navigateTo({
+            url: '/pages/help/help'
+        });
+    },
+    exitLogin: function() {
+        wx.showModal({
+            title: '',
+            confirmColor: '#b4282d',
+            content: '退出登录？',
+            success: function(res) {
+                if (!res.confirm) {
+                    return;
+                }
+
+                util.request(api.AuthLogout, {}, 'POST');
+                app.globalData.hasLogin = false;
+                wx.removeStorageSync('token');
+                wx.removeStorageSync('userInfo');
+                wx.reLaunch({
+                    url: '/pages/index/index'
+                });
+            }
+        })
+
+    }
 })
--- pages/ucenter/sign/sign.js
+++ pages/ucenter/sign/sign.js
@@ -8,260 +8,263 @@ var user = require('../../../utils/user.js');
 var app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    objectId: '',
-    days: [{ isSign: true }],
-    signUp: [18],
-    cur_year: 0,
-    cur_month: 0,
-    count: 10,
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        objectId: '',
+        days: [{ isSign: true }],
+        signUp: [18],
+        cur_year: 0,
+        cur_month: 0,
+        count: 0,
 
-    userInfo: {
-      nickName: '点击登录',
-      avatarUrl: 'http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png'
+        userInfo: {
+            nickName: '点击登录',
+            avatarUrl: 'http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png'
+        },
+        userLevel: '基础会员', //用户等级信息（默认基础会员）
+        order: {
+            unpaid: 0,
+            unship: 0,
+            unrecv: 0,
+            uncomment: 0
+        },
+        hasLogin: false,
+        userGrowValue: 0, //成长值
+        userIntegral: 0, //积分
     },
-    userLevel:'',//用户等级信息
-    order: {
-      unpaid: 0,
-      unship: 0,
-      unrecv: 0,
-      uncomment: 0
-    },
-    hasLogin: false,
-    userGrowValue:0,//成长值
-    userIntegral:0,//积分
-  },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    /*  this.setData({objectId : options.objectId}); */
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        /*  this.setData({objectId : options.objectId}); */
 
-    //获取当前年月  
-    const date = new Date();
-    const cur_year = date.getFullYear();
-    const cur_month = date.getMonth() + 1;
-    const weeks_ch = ['日', '一', '二', '三', '四', '五', '六'];
-    this.calculateEmptyGrids(cur_year, cur_month);
-    this.calculateDays(cur_year, cur_month);
-    //获取当前用户当前任务的签到状态
-    this.onGetSignUp();
+        //获取当前年月  
+        const date = new Date();
+        const cur_year = date.getFullYear();
+        const cur_month = date.getMonth() + 1;
+        const weeks_ch = ['日', '一', '二', '三', '四', '五', '六'];
+        this.calculateEmptyGrids(cur_year, cur_month);
+        this.calculateDays(cur_year, cur_month);
+        //获取当前用户当前任务的签到状态
+        this.onGetSignUp();
 
-    this.setData({
-      cur_year,
-      cur_month,
-      weeks_ch
-    })
-    console.log(this.data.days)
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-     //获取用户的登录信息
-     if (app.globalData.hasLogin) {
-      let userInfo = wx.getStorageSync('userInfo'); //获取用户信息，微信封装
-      let userLevel=wx.getStorageSync('userLevel') //获取用户等级
-      
-      this.setData({
-        userInfo: userInfo,
-        hasLogin: true,
-        userLevel:userLevel
-      });
-        console.log(userInfo)
-        console.log(userLevel)
-      let that = this;
-      util.request(api.UserIndex).then(function(res) {
-        if (res.errno === 0) {
-          that.setData({
-            order: res.data.order
-          });
-        }
-         
-      });
-    }
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-  // 获取当月共多少天
-  getThisMonthDays: function (year, month) {
-    return new Date(year, month, 0).getDate()
-  },
-
-  // 获取当月第一天星期几
-  getFirstDayOfWeek: function (year, month) {
-    return new Date(Date.UTC(year, month - 1, 1)).getDay();
-  },
-
-  // 计算当月1号前空了几个格子，把它填充在days数组的前面
-  calculateEmptyGrids: function (year, month) {
-    var that = this;
-    //计算每个月时要清零
-    that.setData({ days: [] });
-    const firstDayOfWeek = this.getFirstDayOfWeek(year, month);
-    if (firstDayOfWeek > 0) {
-      for (let i = 0; i < firstDayOfWeek; i++) {
-        var obj = {
-          date: null,
-          isSign: false
-        }
-        that.data.days.push(obj);
-      }
-      this.setData({
-        days: that.data.days
-      });
-      //清空
-    } else {
-      this.setData({
-        days: []
-      });
-    }
-  },
-
-  // 绘制当月天数占的格子，并把它放到days数组中
-  calculateDays: function (year, month) {
-    var that = this;
-    const thisMonthDays = this.getThisMonthDays(year, month);
-    for (let i = 1; i <= thisMonthDays; i++) {
-      var obj = {
-        date: i,
-        isSign: false
-      }
-      that.data.days.push(obj);
-    }
-    this.setData({
-      days: that.data.days
-    });
-  },
-
-  //匹配判断当月与当月哪些日子签到打卡
-  onJudgeSign: function () {
-    var that = this;
-    var signs = that.data.signUp;
-    var daysArr = that.data.days;
-    for (var i = 0; i < signs.length; i++) {
-      var current = new Date(signs[i].date.replace(/-/g, "/"));
-      var year = current.getFullYear();
-      var month = current.getMonth() + 1;
-      var day = current.getDate();
-      day = parseInt(day);
-      for (var j = 0; j < daysArr.length; j++) {
-        //年月日相同并且已打卡
-        if (year == that.data.cur_year && month == that.data.cur_month && daysArr[j].date == day && signs[i].isSign == "今日已打卡") {
-          daysArr[j].isSign = true;
-        }
-      }
-    }
-    that.setData({ days: daysArr });
-  },
-
-  // 切换控制年月，上一个月，下一个月
-  handleCalendar: function (e) {
-    const handle = e.currentTarget.dataset.handle;
-    const cur_year = this.data.cur_year;
-    const cur_month = this.data.cur_month;
-    if (handle === 'prev') {
-      let newMonth = cur_month - 1;
-      let newYear = cur_year;
-      if (newMonth < 1) {
-        newYear = cur_year - 1;
-        newMonth = 12;
-      }
-      this.calculateEmptyGrids(newYear, newMonth);
-      this.calculateDays(newYear, newMonth);
-      this.onGetSignUp();
-      this.setData({
-        cur_year: newYear,
-        cur_month: newMonth
-      })
-    } else {
-      let newMonth = cur_month + 1;
-      let newYear = cur_year;
-      if (newMonth > 12) {
-        newYear = cur_year + 1;
-        newMonth = 1;
-      }
-      this.calculateEmptyGrids(newYear, newMonth);
-      this.calculateDays(newYear, newMonth);
-      this.onGetSignUp();
-      this.setData({
-        cur_year: newYear,
-        cur_month: newMonth
-      })
-    }
-  },
-
-  //获取当前用户该任务的签到数组
-  onGetSignUp: function () {
-    util.request(api.signInList).then(res => {
-      /**
-       * 请求服务端接口舞曲当前用户签到数量
-       * 循环遍历并更改日历上签到数据
-       * */ 
-      let days = this.data.days
-      let data = res.data.signRecord.mask
-      data.forEach((item, index) => {
-        days.forEach((jtem, i) => {
-          if (jtem.date === item) {
-            jtem.isSign = true
-          }
+        this.setData({
+            cur_year,
+            cur_month,
+            weeks_ch
         })
-      })
-      //改变签到状态后赋值
-      this.setData({
-         days: days, 
-         count: res.data.signRecord.continueSignDays,
-         userGrowValue:res.data.userGrowValue,
-         userIntegral:res.data.userIntegral,
-        });
-      console.log(res)
-    })
+        console.log(this.data.days)
+    },
 
-  }
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+        //获取用户的登录信息
+        if (app.globalData.hasLogin) {
+            let userInfo = wx.getStorageSync('userInfo'); //获取用户信息，微信封装
+            let userLevel = wx.getStorageSync('userLevel') //获取用户等级
+
+            this.setData({
+                userInfo: userInfo,
+                hasLogin: true,
+                userLevel: userLevel
+            });
+            console.log(userInfo)
+            console.log(userLevel)
+            let that = this;
+            util.request(api.UserIndex).then(function(res) {
+                if (res.errno === 0) {
+                    that.setData({
+                        order: res.data.order
+                    });
+                }
+
+            });
+        }
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    },
+    // 获取当月共多少天
+    getThisMonthDays: function(year, month) {
+        return new Date(year, month, 0).getDate()
+    },
+
+    // 获取当月第一天星期几
+    getFirstDayOfWeek: function(year, month) {
+        return new Date(Date.UTC(year, month - 1, 1)).getDay();
+    },
+
+    // 计算当月1号前空了几个格子，把它填充在days数组的前面
+    calculateEmptyGrids: function(year, month) {
+        var that = this;
+        //计算每个月时要清零
+        that.setData({ days: [] });
+        const firstDayOfWeek = this.getFirstDayOfWeek(year, month);
+        if (firstDayOfWeek > 0) {
+            for (let i = 0; i < firstDayOfWeek; i++) {
+                var obj = {
+                    date: null,
+                    isSign: false
+                }
+                that.data.days.push(obj);
+            }
+            this.setData({
+                days: that.data.days
+            });
+            //清空
+        } else {
+            this.setData({
+                days: []
+            });
+        }
+    },
+
+    // 绘制当月天数占的格子，并把它放到days数组中
+    calculateDays: function(year, month) {
+        var that = this;
+        const thisMonthDays = this.getThisMonthDays(year, month);
+        for (let i = 1; i <= thisMonthDays; i++) {
+            var obj = {
+                date: i,
+                isSign: false
+            }
+            that.data.days.push(obj);
+        }
+        this.setData({
+            days: that.data.days
+        });
+    },
+
+    //匹配判断当月与当月哪些日子签到打卡
+    onJudgeSign: function() {
+        var that = this;
+        var signs = that.data.signUp;
+        var daysArr = that.data.days;
+        for (var i = 0; i < signs.length; i++) {
+            var current = new Date(signs[i].date.replace(/-/g, "/"));
+            var year = current.getFullYear();
+            var month = current.getMonth() + 1;
+            var day = current.getDate();
+            day = parseInt(day);
+            for (var j = 0; j < daysArr.length; j++) {
+                //年月日相同并且已打卡
+                if (year == that.data.cur_year && month == that.data.cur_month && daysArr[j].date == day && signs[i].isSign == "今日已打卡") {
+                    daysArr[j].isSign = true;
+                }
+            }
+        }
+        that.setData({ days: daysArr });
+    },
+
+    // 切换控制年月，上一个月，下一个月
+    handleCalendar: function(e) {
+        const handle = e.currentTarget.dataset.handle;
+        const cur_year = this.data.cur_year;
+        const cur_month = this.data.cur_month;
+        if (handle === 'prev') {
+            let newMonth = cur_month - 1;
+            let newYear = cur_year;
+            if (newMonth < 1) {
+                newYear = cur_year - 1;
+                newMonth = 12;
+            }
+            this.calculateEmptyGrids(newYear, newMonth);
+            this.calculateDays(newYear, newMonth);
+            this.onGetSignUp();
+            this.setData({
+                cur_year: newYear,
+                cur_month: newMonth
+            })
+        } else {
+            let newMonth = cur_month + 1;
+            let newYear = cur_year;
+            if (newMonth > 12) {
+                newYear = cur_year + 1;
+                newMonth = 1;
+            }
+            this.calculateEmptyGrids(newYear, newMonth);
+            this.calculateDays(newYear, newMonth);
+            this.onGetSignUp();
+            this.setData({
+                cur_year: newYear,
+                cur_month: newMonth
+            })
+        }
+    },
+
+    //获取当前用户该任务的签到数组
+    onGetSignUp: function() {
+        util.request(api.signInList).then(res => {
+            /**
+             * 请求服务端接口舞曲当前用户签到数量
+             * 循环遍历并更改日历上签到数据
+             * */
+            let days = this.data.days
+            let data = res.data.signRecord.mask
+            if (data) {
+                data.forEach((item, index) => {
+                        days.forEach((jtem, i) => {
+                            if (jtem.date === item) {
+                                jtem.isSign = true
+                            }
+                        })
+                    })
+                    //改变签到状态后赋值
+                this.setData({
+                    days: days,
+                    count: res.data.signRecord.continueSignDays,
+                    userGrowValue: res.data.userGrowValue,
+                    userIntegral: res.data.userIntegral,
+                });
+                console.log(res)
+            }
+
+        })
+
+    }
 })
